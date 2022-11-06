d6f7024b6a815cae202380397f745e4bfd9d655a
Vitor-J <joaovitorsoares2019@gmail.com>
2022-11-05 23:03:01 -0300

    new commit

--- main.js
+++ main.js
@@ -22,6 +22,28 @@ function createCard(date, day, games) {
   `
 }
 document.querySelector("#cards").innerHTML =
-  createCard("24/12", "Quinta", createGame("brazil", "16:00", "serbia")) +
-  createCard("28/12", "Segunda", createGame("brazil", "13:00", "switzerland")) +
-  createCard("02/12", "Sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("20/11", "Domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "24/11",
+    "Quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southkorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "Segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("southkorea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard(
+    "02/12",
+    "Sexta",
+    createGame("southkorea", "07:00", "portugal") +
+      createGame("ghana", "10:00", "uruguay") +
+      createGame("serbia", "13:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  )
